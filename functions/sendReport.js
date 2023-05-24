69484786fcabddc9f65102930a484a38726fe99a
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-05-24 19:50:57 +0530

    Move to unique filenames

--- functions/sendReport.js
+++ functions/sendReport.js
@@ -1,6 +1,7 @@
 const TG = require("telegram-bot-api");
 const { createImage } = require("./utils/createImage");
 const { createClient } = require("@supabase/supabase-js");
+const nanoid = require("nanoid");
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient(
@@ -19,20 +20,26 @@ const api = new TG({
 });
 
 async function sendImage(chat_id, bufferData, caption = "Here is your generated report!") {
-	await supabase.storage.from("reports").upload(`${chat_id}.png`, bufferData, {
+	const fileName = `${chat_id}-${nanoid()}.png`;
+	await supabase.storage.from("reports").upload(fileName, bufferData, {
 		cacheControl: "0",
 		upsert: true,
 		contentType: "image/png",
 	});
 
-	const { data } = supabase.storage.from("reports").getPublicUrl(`${chat_id}.png`);
-	const publicURL = data.publicUrl;
+	const { data } = await supabase.storage.from("reports").getPublicUrl(fileName);
+	const photo = data.publicUrl;
+	try {
+		await api.sendPhoto({
+			chat_id,
+			caption,
+			photo,
+		});
+	} catch (_err) {
+		console.log("Timeout");
+	}
 
-	await api.sendPhoto({
-		chat_id,
-		caption,
-		photo: publicURL,
-	});
+	await supabase.storage.from("reports").remove(fileName);
 }
 
 exports.handler = async (event) => {
